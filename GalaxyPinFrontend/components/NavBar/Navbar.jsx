a398d4844ab14c923ccbb577784c3a645bf767ec
shrestha-das <shresthadas.0.infinity@gmail.com>
2026-02-14 21:55:44 +0530

    second commit

--- GalaxyPinFrontend/components/NavBar/Navbar.jsx
+++ GalaxyPinFrontend/components/NavBar/Navbar.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import Image from "next/image";
+import Link from "next/link";
 
+// import icon
+import {MdNotification} from "react-icons/md";
+import {BsSearch} from "react-icons/bs";
+import {CgMenuLeft, CgMenuRight} from "react-icons/cg";
 // internal import
 import Style from "./Navbar.module.css";
 
